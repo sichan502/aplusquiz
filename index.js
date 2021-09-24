7236bf9dedc52d6f3d911ea742b74673184102b7
Saoi <marie-502@hotmail.com>
2021-09-23 17:47:34 -0700

    add fetch question

--- index.js
+++ index.js
@@ -1,16 +1,151 @@
+import {quizapi} from './quizapi.js';
+
+let questionlist = [];
+const randombtn = document.querySelector("#random");
+const questionpage = document.querySelector("#question");
+
 const startbtn = document.querySelector("#start");
 const btngroup = document.querySelector("#btngroup");
-const content = document.querySelector("#content")
+const content = document.querySelector("#content");
 
+/* initial */
 startbtn.addEventListener("click", () => {
     startbtn.style.display = "none";
     btngroup.style.display = "inline-block";
 })
 
-const randombtn = document.querySelector("#random");
-const questionpage = document.querySelector("#question");
-
 randombtn.addEventListener("click", () => {
     content.style.display = "none";
     questionpage.style.display = "block";
+    fetchquestion("random", "")
 })
+/* initial */
+
+/* fetch question based on question type or question type and category */
+function fetchquestion(question_type, category)
+{
+    if(question_type == "random")
+    {
+        fetch(`https://quizapi.io/api/v1/questions?apiKey=${quizapi}&limit=10`)
+        .then(response => response.json())
+        .then(data => {
+            questionlist = data;
+            console.log("store data")
+            return questionlist;
+        })
+        .then(qlist => {
+            displayquestion(qlist, 0);
+        })
+    }
+    else /* customize */
+    {
+        fetch(`https://quizapi.io/api/v1/questions?apiKey=${quizapi}&category=${category}&limit=10`)
+        .then(response => response.json())
+        .then(data => {
+            questionlist = data;
+            console.log("store data")
+            return questionlist;
+        })
+    }
+}
+/* fetch question based on question type or question type and category */
+
+/* display question */
+function displayquestion(qlist, qnum)
+{
+    console.log("display question")
+    let out = ``;
+    out = `<div id="question-top">
+                <div id="question-btngroup">
+                    <button id="submit" class="disable">SUBMIT</button>
+                    <p id="question-num">Question ${qnum + 1}</p>
+                    <button id="next" class="disable">Next</button>
+                </div>
+            </div>
+            <div id="question-content">
+                <p>${qlist[qnum].question}</p>
+                <div id="answer-options">
+                    <ul>
+                        <li><button class="options">${qlist[qnum].answers.answer_a}</button></li>
+                        <li><button class="options">${qlist[qnum].answers.answer_b}</button></li>
+                        <li><button class="options">${qlist[qnum].answers.answer_c}</button></li>
+                        <li><button class="options">${qlist[qnum].answers.answer_d}</button></li>
+                        <li><button class="options">${qlist[qnum].answers.answer_e}</button></li>
+                        <li><button class="options">${qlist[qnum].answers.answer_f}</button></li>
+                    </ul>
+                </div>
+            </div>`
+    questionpage.innerHTML = out;
+    setbtn(qlist, ++qnum);
+}
+/* display question */
+
+/* set up button */
+function setbtn(qlist, qnum)
+{
+    const nextbtn = document.querySelector("#next");
+    const submitbtn = document.querySelector("#submit");
+    const optionslist = document.querySelectorAll(".options");
+    /* click submit */
+
+    submitbtn.addEventListener("click", () => {
+        nextbtn.classList.remove("disable");
+        submitbtn.classList.add("disable");
+    })
+
+    /* click submit */
+
+    if(qnum == 10)
+    {
+        nextbtn.style.visibility = "hidden";
+    }
+    else
+    {
+        /* click next */
+        nextbtn.addEventListener("click", () => {
+            console.log("next question");
+
+            displayquestion(qlist, qnum);
+        })
+        /* click next */
+    }
+    
+
+    /* select ans */
+
+    optionslist.forEach(option => {
+        option.addEventListener("click", () => {
+            let curSelect = document.querySelector(".select");
+            if(curSelect)
+            {
+                curSelect.classList.remove("select");
+            }
+            
+            option.classList.add("select");
+            submitbtn.classList.remove("disable")
+        })
+    })
+
+    /* select ans */
+}
+/* set up button */
+
+/* <div id="question-top">
+                <div id="question-btngroup">
+                    <button id="submit" class="disable">SUBMIT</button>
+                    <p id="question-num">Question 1</p>
+                    <button id="next" class="disable">Next</button>
+                </div>
+            </div>
+            <div id="question-content">
+                <p>Which of the following is NOT a valid 
+                    PHP comparison operator?</p>
+                <div id="answer-options">
+                    <ul>
+                        <li><button class="options"><=></button></li>
+                        <li><button class="options"><></button></li>
+                        <li><button class="options">>==</button></li>
+                        <li><button class="options">>=</button></li>
+                    </ul>
+                </div>
+            </div> */
